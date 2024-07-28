c10dd3412f15756223fb1fa424cffda1602dc173
zainab-kassim <kassimzainab11@gmail.com>
2024-07-28 15:14:09 +0100

    second commit

--- app/(root)/layout.jsx
+++ app/(root)/layout.jsx
@@ -1,4 +1,4 @@
-import Footer from "@/components/shared/Footer";
+import Footer from "@components/shared/Footer";
 import Navbar from "@components/shared/navbar";
 
 export default function Layout({ children }) {
